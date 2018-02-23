ba413b06d7bef70f8e6312800f09e414d6a3f5c1
Practice Habits <practicehabits@MacBookAir.local>
2018-02-23 11:26:26 +0700

    crate folder ex1

---
+++ ex1/ex1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+<<<<<<< HEAD
+/*This is a comment. */
+int main(int argc, char *argv[])
+{
+	int distance = 100;
+
+	// this is also a comment
+	printf("You are %d miles away.\n", distance);
+	
+	return 0;
+=======
+/* This is a comment */
+int main(int agrc, char *argv[])
+{
+    int distance = 100;
+
+    // this is also a comment
+    printf("You are %d miles away.\n", distance);
+
+    return 0;
+>>>>>>> 3355337bea80bf7f983bd33eab724283090194f0
+}
