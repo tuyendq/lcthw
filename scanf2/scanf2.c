754713796c78204b240372398f7265cc8d59289d
Tuyen Dang <tuyendq@gmail.com>
2019-01-09 22:50:45 +0700

    T420: add scanf1, scanf3

---
+++ scanf2/scanf2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    printf("Enter two numbers:");
+    scanf("%d %d", &a, &b);
+    printf("\nSum of %d and %d: %d\n", a, b, a+b);
+
+    return 0; 
+}
