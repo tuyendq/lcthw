394c1d5c7d3bbe2eb8f006cfa9b8bb9eb53b3c7f
Tuyen Dang <tuyendq@gmail.com>
2019-05-01 16:54:51 +0700

    add ex15 fibonaci quickcheck

---
+++ quickcheck/quickcheck.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main (){
+   int x = 0, y = 1;
+   if (x || !y)
+  	printf("1");
+   else if (y && x)
+  	printf("2");
+   else
+  	printf("3");
+
+
+   if((10 || (5-2)) && ((6 / 2) - (1 + 2))) {
+   	printf("true");
+   } else {
+   	printf("false");
+   }
+
+}
+
+/*
+int main (){
+   int x = 0, z = 2;
+   while (x < 3) {
+  	printf ("%i ", x);
+      x = x + z;
+    }
+}
+*/
+
+/*
+int foo (int q) {
+    int x = 1;
+    return (q + x);
+}
+int main (){
+   int x = 0;
+   while (x < 3) {
+  	printf ("%i ", x);
+      x = x + foo(x);
+    }
+}
+*/
