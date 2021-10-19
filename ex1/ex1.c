ff6baf5f9c3ab3eb06b0faa55b4365e33d5deec6
Tuyen Dang <tuyendq@gmail.com>
2021-10-19 21:42:45 +0700

    ex1.c: fix conflict

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -1,9 +1,5 @@
 #include <stdio.h>
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 03f182cdc01c5cd82399bae38043c4c41eb13c37
 /*This is a comment. */
 int main(int argc, char *argv[])
 {
@@ -13,18 +9,4 @@ int main(int argc, char *argv[])
 	printf("You are %d miles away.\n", distance);
 	
 	return 0;
-<<<<<<< HEAD
-=======
-=======
-/* This is a comment */
-int main(int agrc, char *argv[])
-{
-    int distance = 100;
-
-    // this is also a comment
-    printf("You are %d miles away.\n", distance);
-
-    return 0;
->>>>>>> 3355337bea80bf7f983bd33eab724283090194f0
->>>>>>> 03f182cdc01c5cd82399bae38043c4c41eb13c37
 }
